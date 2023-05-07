89295ad1543cab943894835dcddfee9141ba5131
Colstin Donaldson <colstin50@gmail.com>
2023-05-07 12:35:21 -0700

    Initial Commit

---
+++ DessertRecipes/DessertRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertRecipesApp.swift
+//  DessertRecipes
+//
+//  Created by Colstin Donaldson on 5/7/23.
+//
+
+import SwiftUI
+
+@main
+struct DessertRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
